53245caa5e93efcb195c055988022c544cad9688
Trung <trung@t7.sg>
2023-09-20 02:38:04 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='py-datasource',
+      version='0.1',
+      description='Crawl data from many data sources',
+      url='https://github.com/TsevenSG/py-datasource',
+      author='Trung',
+      author_email='trung@t7.sg',
+      license='MIT',
+      packages=['t7_datasource'],
+      install_requires=[
+      ],
+      zip_safe=False)
