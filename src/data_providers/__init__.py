dc0bab06d2ae520b59781df4377f7bf836e1892f
Trung Bui <trung@t7.sg>
2023-11-21 15:01:34 +0700

    refactor init 1 more time

--- src/data_providers/__init__.py
+++ src/data_providers/__init__.py
@@ -3,9 +3,9 @@
 from logging import INFO, basicConfig
 from sys import stdout
 
+from data_providers.containers import Container
 from data_providers.data_provider import DataProvider
 from data_providers.enums import *
-from data_providers.models import *
 
 basicConfig(
     level=INFO,
