d7e767ad2cc4ed2d199020506cdb8f72b8f8b7d3
Trung Bui <trung@t7.sg>
2024-04-18 14:21:17 +0700

    do not force pandas 2.1.1 when install as lib

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(name='py_datasource',
       },
       install_requires=[
           'dependency-injector==4.41.0',
-          'pandas==2.1.1',
           'pydantic==2.6.4',
           'websocket-client==1.6.3',
       ],
