bc06e49a26a1859a4a433f6a352cc97dd7d62a55
Trung Bui <trung@t7.sg>
2024-08-28 16:08:18 +0700

    fix wrong type of column symbol

--- src/tradingview/tradingview.py
+++ src/tradingview/tradingview.py
@@ -465,7 +465,7 @@ def _parse_bar_charts(ws, sess_completed: Dict[str, Dict[str, bool]]) -> pd.Data
                     s = s_dict.get(_sess)
                     st = st_dict.get(_sess)
                     _df = pd.concat([s, st], axis=1)
-                    _df['symbol'] = _symbol
+                    _df['symbol'] = pd.Series(repeat(_symbol, len(_df)), dtype='string')
                     _df = _df.reset_index().set_index(['timestamp', 'symbol'])
                     dfs.append(_df)
 
